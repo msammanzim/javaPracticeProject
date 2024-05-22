5443ada45db473b7e0a0836efd390f8fc0b36953
Cliqpack <zimadro@gmail.com>
2024-05-22 20:54:36 +0600

    first commit

---
+++ src/Assignment_1/Identifier.java
@@ -0,0 +1,27 @@
+package Assignment_1;
+
+public class Identifier {
+    int number=25;
+    int _number=30;
+    int Number=35;
+    int nUmBeR=40;
+    int number_45=45;
+    int number$=50;
+    int Number_new_$100=100;
+
+    public void _identifire(){
+
+    }
+    private void identifire1(){
+
+    }
+    protected void $_identifire(){
+
+    }
+    public Identifier(){
+
+    }
+
+
+
+}
